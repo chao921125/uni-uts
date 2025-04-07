e974cd40a5370bfd296fd4bdfa8777d6528a09ff
hc precog <hcprecog@gmail.com>
2025-04-07 12:46:06 +0800

    format

--- env.js
+++ env.js
@@ -1,39 +1,39 @@
 // 自动化测试
 module.exports = {
-    "is-custom-runtime": false,
-    "compile": true,
-    "h5": {
-        "options": {
-            "headless": true
-        },
-        "executablePath": ""
+  "is-custom-runtime": false,
+  "compile": true,
+  "h5": {
+    "options": {
+      "headless": true
     },
-    "mp-weixin": {
-        "port": 9420,
-        "account": "",
-        "args": "",
-        "cwd": "",
-        "launch": true,
-        "teardown": "disconnect",
-        "remote": false,
-        "executablePath": ""
+    "executablePath": ""
+  },
+  "mp-weixin": {
+    "port": 9420,
+    "account": "",
+    "args": "",
+    "cwd": "",
+    "launch": true,
+    "teardown": "disconnect",
+    "remote": false,
+    "executablePath": ""
+  },
+  "app-plus": {
+    "android": {
+      "id": "",
+      "executablePath": ""
     },
-    "app-plus": {
-        "android": {
-            "id": "",
-            "executablePath": ""
-        },
-        "version": "",
-        "ios": {
-            "id": "",
-            "executablePath": ""
-        },
-        "uni-app-x": {
-            "version": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniappx-launcher/base/version.txt",
-            "android": {
-                "id": "emulator-5554",
-                "executablePath": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniappx-launcher/base/android_base.apk"
-            }
-        }
+    "version": "",
+    "ios": {
+      "id": "",
+      "executablePath": ""
+    },
+    "uni-app-x": {
+      "version": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniappx-launcher/base/version.txt",
+      "android": {
+        "id": "emulator-5554",
+        "executablePath": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniappx-launcher/base/android_base.apk"
+      }
     }
+  }
 }
